b89162ad0acb814c126e8f9528a9ad6c68f1c90e
Alllex202 <Alllex.202@gmail.com>
2021-12-06 18:59:08 +0500

    removed unnecessary sorting

--- src/shared/data/sort-items.ts
+++ src/shared/data/sort-items.ts
@@ -9,8 +9,6 @@ interface sortItem {
 export const sortItemsCallersBaseList: sortItem[] = [
     {sortBy: 'CREATION_DATE', direction: 'DESC', text: 'Сначала новые'},
     {sortBy: 'CREATION_DATE', direction: 'ASC', text: 'Сначала старые'},
-    {sortBy: 'COUNT_VARIABLES', direction: 'DESC', text: 'Много переменных'},
-    {sortBy: 'COUNT_VARIABLES', direction: 'ASC', text: 'Мало переменных'},
     {sortBy: 'NAME', direction: 'ASC', text: 'От А до Я'},
     {sortBy: 'NAME', direction: 'DESC', text: 'От Я до А'},
 ];
