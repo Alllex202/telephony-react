51f125d65d5f12aa4d45781665cd3239b98bf783
Alllex202 <Alllex.202@gmail.com>
2021-12-07 17:42:03 +0500

    upd configs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,7 @@
+// create file '.env' in root folder
+// and write env variables with prefix 'REACT_APP_'
+// example 'REACT_APP_MY_NEW_VARIABLE'
+
+// if you use real env variables on PC then you should not use predix
+
 export const serverApi = process.env.SERVER_API || process.env.REACT_APP_SERVER_API;
