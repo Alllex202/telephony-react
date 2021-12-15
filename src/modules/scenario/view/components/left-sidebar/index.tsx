0529e2b095cfbddceb05caa9481b54791714cd59
Alllex202 <Alllex.202@gmail.com>
2021-12-15 16:09:05 +0500

    scenario view page - left sidebar

--- src/modules/scenario/view/components/left-sidebar/index.tsx
+++ src/modules/scenario/view/components/left-sidebar/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './styles.module.scss';
 import Icon from 'components/ui-kit/icon';
 import {NodeType} from 'core/api';
+import {classNames} from 'shared/utils';
 
 const ScenarioLeftSidebar = () => {
     const onDragStart = (e: React.DragEvent<HTMLDivElement>, nodeType: NodeType) => {
@@ -12,13 +13,15 @@ const ScenarioLeftSidebar = () => {
     return (
         <div className={styles.leftSidebar}>
             <div className={styles.infoBlock}>
-                <Icon name={'info'} type={'round'} className={styles.infoIcon}/>
+                <Icon name={'redo'} type={'round'} className={styles.infoIcon}/>
                 <div className={styles.infoText}>
-                    Перетаскивайте компоненты на поле и соединяйте их, чтобы построить сценарий.
+                    Перетащите
+                    компоненты
+                    на поле
                 </div>
             </div>
             <div className={styles.elements}>
-                <div className={styles.element} draggable
+                <div className={classNames(styles.element, styles.start)} draggable
                      onDragStart={(e) => onDragStart(e, 'START')}>
                     Старт
                 </div>
@@ -26,7 +29,7 @@ const ScenarioLeftSidebar = () => {
                      onDragStart={(e) => onDragStart(e, 'REPLICA')}>
                     Реплика
                 </div>
-                <div className={styles.element} draggable
+                <div className={classNames(styles.element, styles.finish)} draggable
                      onDragStart={(e) => onDragStart(e, 'FINISH')}>
                     Финиш
                 </div>
