9823860f3004cd9c42dcd993fafb0838d156af66
Alllex202 <Alllex.202@gmail.com>
2022-01-25 20:31:19 +0500

    Some refactor

--- src/shared/hoocks/use-selector-app.ts
+++ src/shared/hoocks/use-selector-app.ts
@@ -1,6 +1,4 @@
 import {useSelector} from 'react-redux'
 import {RootState} from 'store'
 
-export const useSelectorApp = () => useSelector(
-    (state: RootState) => state
-)
+export const useSelectorApp = () => useSelector((state: RootState) => state)
